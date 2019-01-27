4c8d2f389f3d209f7844b655b4e6308a001cec12
Preston's Dell Laptop <preston.hartzell@gmail.com>
2019-01-26 21:14:32 -0600

    click disaster

--- gpiv/gpiv.py
+++ gpiv/gpiv.py
@@ -1,5 +1,10 @@
 import click
 import pdal
+import json
+import math
+import matplotlib.image as mpimg
+import matplotlib.pyplot as plt
+from osgeo import gdal
 
 # workaround for click not returning help if no arguments or options passed
 # https://stackoverflow.com/questions/50442401/how-to-set-the-default-option-as-h-for-python-click
@@ -18,32 +23,77 @@ class DefaultHelp(click.Command):
 
 
 @click.command(cls=DefaultHelp)
-@click.option('-r', '--raster', 
-    nargs=2, 
-    type=click.Path(exists=True, dir_okay=False), 
-    help='<from las file> <to las file>')
-def rasterize(raster):    
-    click.echo(raster[0])
+# @click.option('-r', '--raster',
+#     type=(click.Path(exists=True, dir_okay=False), click.Path(exists=True, dir_okay=False), float), 
+#     help='<from las file> <to las file> <raster size>')
+@click.argument('fromLAS', type=click.Path(exists=True, dir_okay=False))
+@click.argument('toLAS', type=click.Path(exists=True, dir_okay=False))
+@click.argument('rasterSize', type=float)
+def raster(fromLAS, toLAS, rasterSize):       
+
+    # r = raster[2]*math.sqrt(0.5)
+
+    # fromRaster = {
+    #     "pipeline": [
+    #         raster[0],
+    #         {
+    #             "resolution": raster[2],
+    #             "radius": r,
+    #             "filename": "from.tif"
+    #         }
+    #     ]
+    # }
+    # fromRaster = json.dumps(fromRaster)
+
+    # toRaster = {
+    #     "pipeline": [
+    #         raster[1],
+    #         {
+    #             "resolution": raster[2],
+    #             "radius": r,
+    #             "filename": "to.tif"
+    #         }
+    #     ]
+    # }
+    # toRaster = json.dumps(toRaster)
     
-    import json
+    # pipeline = pdal.Pipeline(fromRaster)
+    # pipeline.validate()
+    # pipeline.execute()
 
-    x = {
-        "pipeline": [
-            raster[0],
-            {
-                "resolution": 5,
-                "radius": 7,
-                "filename": "outputfile.tif"
-            }
-        ]
-    }
+    # pipeline = pdal.Pipeline(toRaster)
+    # pipeline.validate()
+    # pipeline.execute()
+   
+    import rasterio
+    import rasterio.plot
+    import matplotlib.pyplot as plt
 
-    print(x)
-    print(type(x))
-    y = json.dumps(x)
-    print(y)
-    print(type(y))
+    image1 = plt.subplot(121)
+    image2 = plt.subplot(122)
+    
+    fromRaster = rasterio.open('from.tif')
+    rasterio.plot.show((fromRaster, 1), ax=image1, title='From')
+    toRaster = rasterio.open('to.tif')
+    rasterio.plot.show((toRaster, 1), ax=image2, title='To')
+    plt.show()
+
+
+    # image1 = plt.subplot(121)
+    # image2 = plt.subplot(122)
+
+    # #read the image files (png files preferred)
+    # img_source1 = mpimg.imread('img1.jpg')
+    # img_source2 = mpimg.imread('img2.jpg')
+    # #put the images into the window
+    # _ = image1.imshow(img_source1)
+    # _ = image2.imshow(img_source2)
+
+    # #hide axis and show window with images
+    # # image1.axis("off")
+    # # image2.axis("off")
+    # plt.show()
 
 
 if __name__ == '__main__':
-    rasterize()
+    raster()
