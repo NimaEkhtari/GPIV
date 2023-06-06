c060ee688e9177da6f14dfb8630d62e8abdb5204
Nima Ekhtari <95905044+NimaEkhtari@users.noreply.github.com>
2023-06-05 23:21:06 -0500

    update 2
    Added more simplifications to partial derivative calculations and TIN interpolation formula.
    Added a function to interpolate grid using TIN.

--- rasteriztion_functions.py
+++ rasteriztion_functions.py
@@ -2,6 +2,19 @@
 """
 Created on Fri Jun  2 11:07:46 2023
 
+Developed at NCALM, University of Houston.
+
+This code is developed based on original effort by Luyen Bui:
+    https://orcid.org/0000-0003-1091-5573
+
+Based on the following paper:
+    https://doi.org/10.1109/LGRS.2021.3134587
+    
+Partial Derivatives esplained in the supplementary material of above paper at:
+    https://doi.org/10.1109/LGRS.2021.3134587/mm1
+
+
+
 @author: nekhtari
 """
 
@@ -15,19 +28,26 @@ from os.path import abspath as abspath
 import rasterio
 import time
 import pdal
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d.art3d import Poly3DCollection
+
 #from temp_funcs import TIN_lin_Err_Prop_many_MP_starmap2Async
 
 
 def TIN_lin_Err_Prop_many_MP_starmap2Async(ptcloud, ptcloudsigmcov, grdpt, nprocs=mp.cpu_count()): # Error propagation from point cloud's uncertainties to interpolated pts by mp.starmap2Async
     grdtricord, grdtrisimplices = TIN_grdtricord_simplices(ptcloud, grdpt)
-    parderiv = TIN_lin_par_deriv(grdtricord, grdpt)    # Partial derivatives of the Z fn at grdpt w.r.t coords of triangles' vertices from ptcloud:
-                                                                                            # [ngrdpt, nparderiv=9] = [no of grid pts, number of partial derivatives (9: dzp/dx1, ..., dzp/dz3)]
+    
+    centroid = np.mean(ptcloud, axis = 0)
+    TIN_coeffs, parderiv = TIN_lin_par_deriv(grdtricord, grdpt, centroid)    # calculate TIN equation coefficients and it's partial derivatives w.r.t coordinates of 3 vertices
+    
+    interpolated_grid = Interpolate_TIN(grdpt, TIN_coeffs, centroid)  # Calculate interpolated heights
+    
     del grdtricord
     inp_list_idx = slice_data_idx(len(grdpt), nprocs)
     inp_grdtrisimplices, inp_parderiv = [[grdtrisimplices[item, :] for item in row] for row in inp_list_idx], [[parderiv[item] for item in row] for row in inp_list_idx]
     with mp.Pool(nprocs) as pool: results = pool.starmap_async(TIN_lin_Err_Prop_many, [(ptcloudsigmcov, inp_grdtrisimplices[ii], inp_parderiv[ii], ii+1) for ii in range(len(inp_list_idx))]).get()    # this calls pool.starmap_async, normally faster than pool.starmap
     grdpterr = [item for sublist in results for item in sublist]
-    return np.array(grdpterr, dtype=float)
+    return (interpolated_grid, np.array(grdpterr, dtype=float))
 
 
 def TIN_grdtricord_simplices(ptcloud, grdpt): # Identify coordinates of vertices of TIN triangles formed from ptcloud where grdpt are located inside
@@ -60,10 +80,10 @@ def TIN_grdtricord_simplices(ptcloud, grdpt): # Identify coordinates of vertices
     return (grdtricord, grdtrisimplices)
 
 
-def TIN_lin_par_deriv   (grdtricord, grdpt): # Compute partial derivatives of the Z function at interpolated points (Zp) w.r.t coords (x,y,z) of three triangle's vertices in TIN linear interpolation
+def TIN_lin_par_deriv   (tv, grdpt, centroid): # Compute partial derivatives of the Z function at interpolated points (Zp) w.r.t coords (x,y,z) of three triangle's vertices in TIN linear interpolation
     '''
     input:
-        grdtricord: Coords of triangles' vertices
+        tv: Coords of triangles' vertices
             Format: [ngrdpt, nvertex=3, ncord=3] = [no of grid pts, no of vertices (3), no of coordinates (3: X, Y, Z)]
             Type:   array
         grdpt: Coords of grid pts
@@ -73,29 +93,34 @@ def TIN_lin_par_deriv   (grdtricord, grdpt): # Compute partial derivatives of th
         TIN_par_deriv:
             Format: [ngrdpt, noparderiv=9] = [no of grid pts, number of partial derivatives (9: dzp/dx1, ..., dzp/dz3)]
             Type:   array '''
-    if grdtricord.shape[0] != grdpt.shape[0]: sys.exit("TIN_lin_par_deriv: 'grdtricord' and 'grdpt' must have the same number of elements.")
-    TIN_coefs = TIN_lin_coefs_est(grdtricord) # [ngrdpt, ncoefs=4] = [no of grid pts, no of coefficients (4: A, B, C, D)]
+    if tv.shape[0] != grdpt.shape[0]: sys.exit("TIN_lin_par_deriv: 'tv' and 'grdpt' must have the same number of elements.")
+    
+    tv = tv - centroid
+    
+    TIN_coefs = TIN_lin_coefs_est(tv) # [ngrdpt, ncoefs=4] = [no of grid pts, no of coefficients (4: A, B, C, D)]
     tic, TIN_par_deriv = time.time(), []
     for idx, grdptcord in enumerate(grdpt):
-        sub_par_deriv = np.full(9, np.nan, dtype=float) # if the considered grid pt is NOT located in any TIN triangles then all 'grdtricord' corresponding to this pt are NaN, and all 'sub_par_deriv' are also NaN
-        if not np.isnan(grdtricord[idx]).any(): # if the considered grid pt is located in a TIN triangle then do as below
-            x1, y1, z1, x2, y2, z2, x3, y3, z3 = grdtricord[idx,0,0], grdtricord[idx,0,1], grdtricord[idx,0,2], grdtricord[idx,1,0], grdtricord[idx,1,1], grdtricord[idx,1,2], grdtricord[idx,2,0], grdtricord[idx,2,1], grdtricord[idx,2,2]
+        d = np.full(9, np.nan, dtype=float) # if the considered grid pt is NOT located in any TIN triangles then all 'tv' corresponding to this pt are NaN, and all 'sub_par_deriv' are also NaN
+        if not np.isnan(tv[idx]).any(): # if the considered grid pt is located in a TIN triangle then do as below
+            x1, y1, z1, x2, y2, z2, x3, y3, z3 = tv[idx,0,0], tv[idx,0,1], tv[idx,0,2], tv[idx,1,0], tv[idx,1,1], tv[idx,1,2], tv[idx,2,0], tv[idx,2,1], tv[idx,2,2]
             A, B, C, D = TIN_coefs[idx,0], TIN_coefs[idx,1], TIN_coefs[idx,2], TIN_coefs[idx,3]
             xp, yp = grdptcord[0], grdptcord[1]
-            sub_par_deriv[0] = 1/(C**2)*((y3-y2)*xp*A+(y3-y2)*yp*B+((z2-z3)*yp+(y2*z3-y3*z2))*C+(y3-y2)*D)
-            sub_par_deriv[1] = 1/(C**2)*((x2-x3)*xp*A+(x2-x3)*yp*B+((z3-z2)*xp+(x3*z2-x2*z3))*C+(x2-x3)*D)
-            sub_par_deriv[2] = 1/C*((y2-y3)*xp+(x3-x2)*yp+(x2*y3-x3*y2))
-            sub_par_deriv[3] = 1/(C**2)*((y1-y3)*xp*A+(y1-y3)*yp*B+((z3-z1)*yp+(y3*z1-y1*z3))*C+(y1-y3)*D)
-            sub_par_deriv[4] = 1/(C**2)*((x3-x1)*xp*A+(x3-x1)*yp*B+((z1-z3)*xp+(x1*z3-x3*z1))*C+(x3-x1)*D)
-            sub_par_deriv[5] = 1/C*((y3-y1)*xp+(x1-x3)*yp+(x3*y1-x1*y3))
-            sub_par_deriv[6] = 1/(C**2)*((y2-y1)*xp*A+(y2-y1)*yp*B+((z1-z2)*yp+(y1*z2-y2*z1))*C+(y2-y1)*D)
-            sub_par_deriv[7] = 1/(C**2)*((x1-x2)*xp*A+(x1-x2)*yp*B+((z2-z1)*xp+(x2*z1-x1*z2))*C+(x1-x2)*D)
-            sub_par_deriv[8] = 1/C*((y1-y2)*xp+(x2-x1)*yp+(x1*y2-x2*y1))
-        TIN_par_deriv.append(sub_par_deriv)
+            C2 = C ** 2
+            E = ((xp * A) + (yp * B) + D)
+            d[0] = ((y3 - y2) * E + (((z2 - z3) * yp) + ((y2 * z3) - (y3 * z2))) * C) / C2
+            d[3] = ((y1 - y3) * E + (((z3 - z1) * yp) + ((y3 * z1) - (y1 * z3))) * C) / C2
+            d[6] = ((y2 - y1) * E + (((z1 - z2) * yp) + ((y1 * z2) - (y2 * z1))) * C) / C2
+            d[1] = ((x2 - x3) * E + (((z3 - z2) * xp) + ((x3 * z2) - (x2 * z3))) * C) / C2
+            d[4] = ((x3 - x1) * E + (((z1 - z3) * xp) + ((x1 * z3) - (x3 * z1))) * C) / C2
+            d[7] = ((x1 - x2) * E + (((z2 - z1) * xp) + ((x2 * z1) - (x1 * z2))) * C) / C2
+            d[2] = (((y2 - y3) * xp) + ((x3 - x2) * yp) + ((x2 * y3) - (x3 * y2))) / C
+            d[5] = (((y3 - y1) * xp) + ((x1 - x3) * yp) + ((x3 * y1) - (x1 * y3))) / C
+            d[8] = (((y1 - y2) * xp) + ((x2 - x1) * yp) + ((x1 * y2) - (x2 * y1))) / C
+        TIN_par_deriv.append(d)
 
-        toctime = time.time() - tic
-        print('Taking partial derivatives took {} seconds'.format(toctime))
-    return np.array(TIN_par_deriv, dtype=float)
+    toctime = time.time() - tic
+    print('Taking partial derivatives took {} seconds'.format(toctime))
+    return (TIN_coefs, np.array(TIN_par_deriv, dtype=float))
 
 
 def TIN_lin_coefs_est(tricord): # Estimate coefficients A, B, C, and D in triangular (TIN) linear interpolation
@@ -113,10 +138,12 @@ def TIN_lin_coefs_est(tricord): # Estimate coefficients A, B, C, and D in triang
         if np.isnan(elem).any(): # If the considered pt is not located in any TIN triangles then all 'tricord' corresponding to this pt is NaN, and all 'TIN_coefs' are also NaN
             TIN_coefs.append(np.full(4, np.nan, dtype=float))
         else:
-            A = elem[0,1]*elem[2,2]-elem[0,1]*elem[1,2]+elem[1,1]*elem[0,2]-elem[1,1]*elem[2,2]+elem[2,1]*elem[1,2]-elem[2,1]*elem[0,2]
-            B = elem[0,0]*elem[1,2]-elem[0,0]*elem[2,2]+elem[1,0]*elem[2,2]-elem[1,0]*elem[0,2]+elem[2,0]*elem[0,2]-elem[2,0]*elem[1,2]
-            C = elem[0,0]*elem[1,1]-elem[0,0]*elem[2,1]+elem[1,0]*elem[2,1]-elem[1,0]*elem[0,1]+elem[2,0]*elem[0,1]-elem[2,0]*elem[1,1]
-            D = elem[0,0]*elem[1,1]*elem[2,2]-elem[0,0]*elem[2,1]*elem[1,2]+elem[1,0]*elem[2,1]*elem[0,2]-elem[1,0]*elem[0,1]*elem[2,2]+elem[2,0]*elem[0,1]*elem[1,2]-elem[2,0]*elem[1,1]*elem[0,2]
+            x, y, z = elem[:,0], elem[:,1], elem[:,2]
+            o = np.ones(x.shape)
+            A = np.linalg.det(np.column_stack([z, y, o]))
+            B = np.linalg.det(np.column_stack([x, z, o]))
+            C = np.linalg.det(np.column_stack([x, y, o]))
+            D = np.linalg.det(np.column_stack([x, y, z]))
             TIN_coefs.append([A, B, C, D])
     return np.array(TIN_coefs, dtype=float)
 
@@ -179,6 +206,40 @@ def TIN_lin_Err_Prop_many(ptcloudsigmcov, grdtrisimplices, parderiv, worker=1): 
 
 
 
+def Interpolate_TIN(gp, coeff, centroid):
+    '''
+    input:
+        gp: numpy array holding X and Y coordinates of grid points
+            format: [ngrdpt, nvertex=3, ncord=2] = [no of grid pts, no of vertices=3, no of coordinates=2 (X,Y)]
+            type: array
+        coeff: TIN equation coefficients to calculate Z for any given point within TIN triangle
+            format: [nTriangles, 4] = [A, B, C, D]
+            type: array
+        centroid: The centroid of grid points (or data, doesn't matter)
+            
+        output:
+            Zp: Interpolated height using TIN
+                format: [ngrdpt, ]
+                type: array
+            
+            Using the following formula:
+                a = A / C
+                b = B / C
+                c = D / C
+                Zp = (a * Xp) + (b * Yp) + c
+    '''
+    
+    a = coeff[:, 0] / coeff[:, 2]
+    b = coeff[:, 1] / coeff[:, 2]
+    c = coeff[:, 3] / coeff[:, 2]
+    
+    Xp = gp[:, 0] - centroid[0]
+    Yp = gp[:, 1] - centroid[1]
+    
+    Zp = (a * Xp) + (b * Yp) + c + centroid[2]
+    return Zp
+
+
 
 
 ''' ----------------------------------------------------------------------- '''
@@ -224,11 +285,27 @@ mingrdY, grddY, maxgrdY = np.ceil(miny), grid_size, np.floor(maxy)
 grdX ,  grdY = np.arange(mingrdX, maxgrdX+grddX, grddX), np.arange(mingrdY, maxgrdY+grddY, grddY)
 meshX, meshY = np.meshgrid(grdX, grdY)
 grdpt = np.array(np.column_stack((meshX.ravel(), meshY.ravel())))
-del mingrdX, grddX, maxgrdX, mingrdY, grddY, maxgrdY, grdX, grdY, meshX, meshY
+del mingrdX, grddX, maxgrdX, mingrdY, grddY, maxgrdY, grdX, grdY
 
 
 
-Z = TIN_lin_Err_Prop_many_MP_starmap2Async(ptcloud, sig_COV, grdpt)
+Z, dZ = TIN_lin_Err_Prop_many_MP_starmap2Async(ptcloud, sig_COV, grdpt)
+
+
+ZZ = Z.reshape(meshX.shape)
+fig = plt.figure()
+plt.imshow(np.flipud(ZZ))
+
+DZ = dZ.reshape(meshX.shape)
+fig = plt.figure()
+plt.imshow(np.flipud(ZZ))
+
+
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+ax.plot_surface(meshX, meshY, ZZ, alpha=0.5, color='blue', label='DEM')
+
+
 
 out_tiff = r'./tpu.tif'
 new_dataset = rasterio.open(out_tiff, 'w', driver='GTiff', height=Z.shape[0],
